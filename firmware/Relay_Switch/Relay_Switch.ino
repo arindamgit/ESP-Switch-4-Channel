b2c496a0f60b1dd7dee3d31f00b77530e3edf576
arindam das <arindam.gtalk@gmail.com>
2021-10-31 22:52:46 +0530

    First commit - on/off endpoint works

---
+++ firmware/Relay_Switch/Relay_Switch.ino
@@ -0,0 +1,214 @@
+/**
+   Firmware for 4-way switch driven by ESP-01
+*/
+#include <ArduinoJson.h>
+#include "FS.h"
+#include <LittleFS.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+#include <WiFiClient.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+
+
+
+ESP8266WiFiMulti WiFiMulti;
+
+#define HTTP_REST_PORT 8080
+AsyncWebServer httpRestServer(HTTP_REST_PORT);
+
+const byte switch_01 = 0; // GPIO-0
+const byte switch_02 = 2; // GPIO-2
+const byte switch_03 = 1; // TX
+const byte switch_04 = 3; // RX
+
+// const char* ssid; const char* password; // read these from config later
+
+const char* ssid = "M K Das Jio";
+const char* password = "Kalyani3120";
+
+
+void setup() {
+  Serial.begin(115200);
+  //initFS();
+  manageConnection();
+}
+
+void loop() {
+  //   Serial.println("Looping - OK");
+//  httpRestServer.handleClient();
+}
+
+/**
+   Setup the connectivity for the device
+   Firstly try to read configuration to get router configuration and connect in station-mode
+   If it cannot find or connect to a suitable router then the ESP needs to be reconfigured with an external device
+   At this point configure ESP to dive into AP-mode
+*/
+void manageConnection() {
+  if (connectToRouter()) {
+    Serial.println("Connection established");
+    Serial.println(WiFi.localIP());
+    pinConfiguration();
+    initOutputs();
+    setupRoutes();
+    httpRestServer.begin();
+  } else {
+    Serial.println("Router not connected");
+  }
+  /*
+    if (isSoftReset()) {
+    setupAPMode();
+    } else {
+    if (loadWiFiConfig()) {
+      if (!connectToRouter()) {
+        setupAPMode();
+        resetDevice();
+      } else {
+        pinConfiguration();
+        initOutputs();
+        setupRoutes();
+        httpRestServer.begin();
+      }
+    } else {
+      setupAPMode();
+      resetDevice();
+    }
+    }*/
+}
+
+void setupRoutes() {
+  // Send a GET request to <ESP_IP>/on?switch=<switchId>
+  httpRestServer.on("/on", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    String switchId;
+
+    if (request->hasParam("switch")) {
+      switchId = request->getParam("switch")->value();
+      turnOn(switchId.toInt());
+    }
+    request->send(200, "text/plain", "OK");
+  });
+
+  // Send a GET request to <ESP_IP>/off?switch=<switchId>
+  httpRestServer.on("/off", HTTP_GET, [] (AsyncWebServerRequest * request) {
+    String switchId;
+
+    if (request->hasParam("switch")) {
+      switchId = request->getParam("switch")->value();
+      turnOff(switchId.toInt());
+    }
+    request->send(200, "text/plain", "OK");
+  });
+}
+
+bool isSoftReset() {
+  int reason = ESP.getResetInfoPtr()->reason;
+  bool softReset = false;
+  switch (reason) {
+    case REASON_SOFT_RESTART:
+      softReset = true;
+      break;
+    default:
+      break;
+  }
+  return softReset;
+}
+void setupAPMode() {
+  Serial.println("Setting up AP mode");
+  WiFi.softAP("AD Automation", "11111111");
+}
+
+void resetDevice() {
+  Serial.println("resetting esp");
+  ESP.restart();
+}
+/**
+   Connects to WiFi router when available
+*/
+bool connectToRouter() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  int attempts = 1;
+  while (WiFi.status() != WL_CONNECTED && attempts < 20) {
+    delay(1000);
+    attempts++;
+  }
+  return WiFi.status() == WL_CONNECTED;
+}
+/**
+   Reads SSID and Password from config file
+*/
+bool loadWiFiConfig() {
+  // test code //
+  // ssid = "M K Das Jio";
+  // password = "Kalyani3120";
+  return true;
+  /*
+    File configFile = LittleFS.open("/config.json", "r");
+    if (!configFile) {
+    Serial.println("loadWiFiConfig: no config file");
+    return false;
+    }
+
+    size_t size = configFile.size();
+    if (size > 1024) {
+    Serial.println("loadWiFiConfig: config too large");
+    return false;
+    }
+
+    // Allocate a buffer to store contents of the file.
+    std::unique_ptr<char[]> buf(new char[size]);
+
+    // We don't use String here because ArduinoJson library requires the input
+    // buffer to be mutable. If you don't use ArduinoJson, you may as well
+    // use configFile.readString instead.
+    configFile.readBytes(buf.get(), size);
+
+    StaticJsonDocument<200> doc;
+    DeserializationError error = deserializeJson(doc, buf.get());
+    if (error) {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.f_str());
+    return false;
+    }
+
+    ssid = doc["ssid"];
+    password = doc["password"];
+    return true;
+  */
+}
+/**
+   Configure the GPIO pins (including RX and TX) is output mode
+*/
+void pinConfiguration() {
+  pinMode(switch_01, OUTPUT);
+  pinMode(switch_02, OUTPUT);
+  pinMode(switch_03, OUTPUT);
+  pinMode(switch_04, OUTPUT);
+}
+
+/**
+   Turn off all GPIO outputs
+*/
+void initOutputs() {
+  digitalWrite(switch_01, LOW);
+  digitalWrite(switch_02, LOW);
+  digitalWrite(switch_03, LOW);
+  digitalWrite(switch_04, LOW);
+}
+/*
+  void initFS() {
+  if (!LittleFS.begin()) {
+    Serial.println("Failed to mount file system");
+    return;
+  }
+  }
+*/
+
+void turnOn(int switchId) {
+  digitalWrite(switchId, HIGH);
+}
+
+void turnOff(int switchId) {
+  digitalWrite(switchId, LOW);
+}
